8990b49cf0426a1a333fc28d9e0229a04b6e6758
Patrick Wolleb <patrick.wolleb@evrythng.com>
2016-03-01 13:17:28 +0000

    Adds mongodb driver
    Adds docker compose
    
    Adds editorconfig

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ ADD . $APP_DIR
 
 RUN cd $APP_DIR && npm i
 
+EXPOSE 9000
+
 CMD cd $APP_DIR && npm start 
--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const mongodb = require('mongodb');
+
+
+mongodb.connect('mongodb://docker:27017/jserver', function() {
+	console.log(arguments);
+})
+
+
 require('http').createServer(function(req, res) {
-  	res.end("Hello World!\n");
+  res.end("Hello World!\n");
 }).listen(9000);
