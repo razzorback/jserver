b2ad667326b1447087242ab479952207ea1c965f
Patrick Wolleb <patrick.wolleb@evrythng.com>
2016-03-16 12:18:47 +0000

    Adds basic data structures
    Adds expressjs

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const mongodb = require('mongodb');
+const express = require('express');
+const app = express();
 
 
-mongodb.connect('mongodb://docker:27017/jserver', function() {
-	console.log(arguments);
-})
+
+app.get('/thngs', function(req, res) {
+
+  const arr = []
+  
+  const obj = {
+    patrick : 'Hello',
+    list : [ 'hello', 'world' ]
+  };
+
+  arr.push(obj)
+  res.send(200, arr)
+});
 
 
-require('http').createServer(function(req, res) {
-  res.end("Hello World!\n");
-}).listen(9000);
+app.listen(9000);
+
+
+
+// mongodb.connect('mongodb://docker:27017/jserver', function() {
+//   console.log(arguments);
+// })
+
