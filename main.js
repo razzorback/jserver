6d0e68ede1ce3c4254e9b0dbdea4296509555c38
Patrick Wolleb <patrick.wolleb@evrythng.com>
2016-03-16 13:02:34 +0000

    Adds basic rest server

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
-const mongodb = require('mongodb');
 const express = require('express');
 const app = express();
+app.use(require('body-parser').json())
 
-
+const DB = [];
+var i = 0;
 
 app.get('/thngs', function(req, res) {
-
-  const arr = []
-  
-  const obj = {
-    patrick : 'Hello',
-    list : [ 'hello', 'world' ]
-  };
-
-  arr.push(obj)
-  res.send(200, arr)
+  res.send(200, DB)
 });
 
+app.post('/thngs', function(req, res) {
+  var obj = req.body;
+  const err = validate(obj);
+  if(!err) {
+    DB.push(obj);
+    obj.id = ++i;
+    res.send(201, obj)
+  } else {
+    res.send(400, err);
+  }
+});
 
 app.listen(9000);
 
-
-
-// mongodb.connect('mongodb://docker:27017/jserver', function() {
-//   console.log(arguments);
-// })
-
+function validate(input) { 
+  if(!input) {
+    return {
+      error: 'Missing payload'
+    }
+  } else if(!input.name) {
+    return {
+      error : 'Missing name'
+    }
+  } else if(typeof input.name !== 'string') {
+    return {
+      error : 'Name must be a string'
+    }
+  } else if(input.name.length < 3) {
+    return  {
+      error : 'Name must have at least 3 characters.'
+    } 
+  } 
+  return false
+}
